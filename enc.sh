03408fcac37a8fcfe40d867849dd2126149ccd56
root <root@truenas.local>
2021-04-21 11:58:11 +0800

    check ip change first

--- enc.sh
+++ enc.sh
@@ -2,6 +2,15 @@
 # $1: content  $2:key
 # echo $1 | openssl aes-128-cbc -k $2 -base64 2>/dev/null > encip.txt
 content=$(cat $1  2>/dev/null || echo $1) 
+lastcontent=`./dec.sh $2`
+#echo last:$lastcontent   cur:$content
+if [ "$content" == "$lastcontent" ]; then
+#	echo same
+	exit 0
+fi
+#echo not same
+#exit 0
+
 echo $content | openssl aes-128-cbc -k $2 -base64 2>/dev/null > encip.txt
 git commit -a -m "update ip" 2>&1 >/dev/null
 git push origin master
