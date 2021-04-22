594bd1e693a0b029a1777986cc6b20fb5b416707
root <root@truenas.local>
2021-04-22 10:21:32 +0800

    fix multi ip repeat commit error

--- enc.sh
+++ enc.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # $1: content  $2:key
 # echo $1 | openssl aes-128-cbc -k $2 -base64 2>/dev/null > encip.txt
-content=$(cat $1  2>/dev/null || echo $1) 
-lastcontent=`./dec.sh $2`
-#echo last:$lastcontent   cur:$content
+content="$(cat $1  2>/dev/null || echo $1)" 
+lastcontent="`./dec.sh $2`"
+#echo last:"$lastcontent"   cur:"$content"
 if [ "$content" == "$lastcontent" ]; then
 #	echo same
 	exit 0
@@ -11,6 +11,7 @@ fi
 #echo not same
 #exit 0
 
-echo $content | openssl aes-128-cbc -k $2 -base64 2>/dev/null > encip.txt
+echo "$content" | openssl aes-128-cbc -k $2 -base64 2>/dev/null > encip.txt
+#exit 0
 git commit -a -m "update ip" 2>&1 >/dev/null
 git push origin master
