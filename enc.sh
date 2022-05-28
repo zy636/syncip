b937cd2b8b8661d7bf01e4e6d7026671ceb665b6
root <root@truenas.local>
2022-05-28 21:45:31 +0800

    fix sh

--- enc.sh
+++ enc.sh
@@ -6,12 +6,14 @@ lastcontent="`./dec.sh $2`"
 #echo last:"$lastcontent"   cur:"$content"
 if [ "$content" == "$lastcontent" ]; then
 #	echo same
-	exit 0
-fi
+#	exit 0
+echo same > /dev/null
+else
 #echo not same
 #exit 0
 
 echo "$content" | openssl aes-128-cbc -k $2 -base64 2>/dev/null > encip.txt
 #exit 0
 git commit -a -m "update ip" 2>&1 >/dev/null
+fi
 git push origin master
